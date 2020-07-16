e3825a6700f6941cb470ea4a3e60b6d20c136bc5
Carlos Alabau <carlytos347@gmail.com>
2020-07-16 16:24:08 +0200

    Errores resueltos y funcionando 100%

--- components/graphic/graphic.jsx
+++ components/graphic/graphic.jsx
@@ -1,16 +1,21 @@
 import { LineChart } from "react-native-line-chart";
-import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { ScrollView } from "react-native-gesture-handler";
 
 const graphic = () => {
   const screenWidth = Dimensions.get("window").width;
-
+  const styles = StyleSheet.create({
+    graphic: {
+      marginLeft:2
+    }
+  })
   //Configuracion para el grafico
   const chartConfig = {
     backgroundGradientFrom: "#1E2923",
     backgroundGradientTo: "#08130D",
-    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`
   };
 
   // Aqui guardo los datos que recojo de la API
@@ -18,34 +23,37 @@ const graphic = () => {
 
   // Obtengo y guardo los datos de la API
   const getData = async () => {
-      const res = await axios.get(
-        "https://rithmi-frontend-test.s3-eu-west-1.amazonaws.com/samples.json"
-      );
-      const order = res.data.sort((a, b) =>
-        a.date < b.date ? 1 : a.date > b.date ? -1 : 0
-      );
-      setData(order);
+    const res = await axios.get(
+      "https://rithmi-frontend-test.s3-eu-west-1.amazonaws.com/samples.json"
+    );
+    const order = res.data.sort((a, b) =>
+      a.date < b.date ? 1 : a.date > b.date ? -1 : 0
+    );
+    setData(order);
   };
-  
+
   useEffect(() => {
     getData(); // Llamo a la funcion al cargar la pagina
   }, []);
 
   return (
-    <View>
-      <LineChart
-        data={{
-          datasets: [
-            {
-              data: data.map((el) => el.heartRate),
-            },
-          ],
-        }}
-        width={screenWidth}
-        height={220}
-        chartConfig={chartConfig}
-      />
-    </View>
+    <ScrollView>
+      {data.length > 0 && (
+        <LineChart
+          data={{
+            datasets: [
+              {
+                data: data.map((el) => el.heartRate),
+              },
+            ],
+          }}
+          style={styles.graphic}
+          width={screenWidth/1.01}
+          height={220}
+          chartConfig={chartConfig}
+        />
+      )}
+    </ScrollView>
   );
 };
 
